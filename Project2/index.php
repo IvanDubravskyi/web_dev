3e962e846d0ad0579032514316c0377047fa6432
Ivan Dubravskyi <ivan.dybravskyi@gmail.com>
2019-04-19 10:20:41 +0300

    lab_2

---
+++ Project2/index.php
@@ -0,0 +1,12 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+	<iframe src="menu.html" width="90%" height="300"></iframe><br>
+
+	<iframe src="iframe.html" width="90%" height="300" name="_parent"></iframe><br>
+</body>
+</html>
---
+++ Project2/page10.php
@@ -0,0 +1,10 @@
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?>
+
+<?php
+echo "The time is " . date("h:i:sa");
+?>
